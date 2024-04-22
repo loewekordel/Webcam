2b35a547ccf16d47d63fbd78c2aa08f805f79a45
loewekordel <georg.vietze@gmx.net>
2024-04-23 00:54:17 +0200

    Fix folder structure and incorrect paths

--- camserver.py
+++ camserver.py
@@ -1,4 +1,4 @@
-import picamera2 #camera module for RPi camera
+import picamera2  # camera module for RPi camera
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, MJPEGEncoder
 from picamera2.outputs import FileOutput, CircularOutput
@@ -11,28 +11,38 @@ import atexit
 from datetime import datetime
 from threading import Condition
 import time
-import os
+from pathlib import Path
 
 from libcamera import Transform
 
-app = Flask(__name__, template_folder='template', static_url_path='/static')
+OUTPUT_DIR = Path(__file__).parent / "static"
+OUTPUT_DIR_VIDEO = OUTPUT_DIR / "video"
+OUTPUT_DIR_PICTURES = OUTPUT_DIR / "pictures"
+OUTPUT_DIR_SOUND = OUTPUT_DIR / "sound"
+
+app = Flask(__name__, template_folder="template", static_url_path="/static")
 api = Api(app)
 
 encoder = H264Encoder()
 output = CircularOutput()
 
+
 class Camera:
     def __init__(self):
         self.camera = picamera2.Picamera2()
-        self.camera.configure(self.camera.create_video_configuration(main={"size": (800, 600)}))
+        self.camera.configure(
+            self.camera.create_video_configuration(main={"size": (800, 600)})
+        )
         self.still_config = self.camera.create_still_configuration()
         self.encoder = MJPEGEncoder(10000000)
         self.streamOut = StreamingOutput()
         self.streamOut2 = FileOutput(self.streamOut)
         self.encoder.output = [self.streamOut2]
+        self.output_dir_video = OUTPUT_DIR_VIDEO
+        self.output_dir_pictures = OUTPUT_DIR_PICTURES
 
-        self.camera.start_encoder(self.encoder) 
-        self.camera.start_recording(encoder, output) 
+        self.camera.start_encoder(self.encoder)
+        self.camera.start_recording(encoder, output)
 
     def get_frame(self):
         self.camera.start()
@@ -40,17 +50,20 @@ class Camera:
             self.streamOut.condition.wait()
             self.frame = self.streamOut.frame
         return self.frame
-    
+
     def VideoSnap(self):
         print("Snap")
-        timestamp = datetime.now().isoformat("_","seconds")
+        timestamp = datetime.now().isoformat("_", "seconds")
         print(timestamp)
         self.still_config = self.camera.create_still_configuration()
-        self.file_output = "/home/allzero22/Webserver/webcam/static/pictures/snap_%s.jpg" % timestamp
+        self.file_output = self.output_dir_pictures / f"snap_{timestamp}.jpg"
         time.sleep(1)
-        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
+        self.job = self.camera.switch_mode_and_capture_file(
+            self.still_config, self.file_output, wait=False
+        )
         self.metadata = self.camera.wait(self.job)
-    
+
+
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
         self.frame = None
@@ -61,92 +74,107 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
-#defines the function that generates our frames
+
+# defines the function that generates our frames
 camera = Camera()
 
-#capture_config = camera.create_still_configuration()
+
+# capture_config = camera.create_still_configuration()
 def genFrames():
     while True:
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-            
-#defines the route that will access the video feed and call the feed function
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+
+# defines the route that will access the video feed and call the feed function
 class VideoFeed(Resource):
     def get(self):
-        return Response(genFrames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-    
+        return Response(
+            genFrames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+        )
+
+
 # Timestamp
 def show_time():
-    ''' Show current date time in text format '''
+    """Show current date time in text format"""
     rightNow = datetime.now()
     print(rightNow)
     currentTime = rightNow.strftime("%d-%m-%Y_%H:%M:%S")
     print("date and time =", currentTime)
-        
+
     return currentTime
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """Video streaming home page."""
-    
-    return render_template('index.html')
 
-@app.route('/home', methods = ['GET', 'POST'])
-def home_func():
-    """Video streaming home page."""
-    
     return render_template("index.html")
 
-@app.route('/info.html')
+
+@app.route("/home", methods=["GET", "POST"])
+def home_func():
+    """Video streaming home page."""
+
+    return render_template("index.html")
+
+
+@app.route("/info.html")
 def info():
     """Info Pane"""
-    
-    return render_template('info.html')
 
-@app.route('/startRec.html')
+    return render_template("info.html")
+
+
+@app.route("/startRec.html")
 def startRec():
     """Start Recording Pane"""
     print("Video Record")
     basename = show_time()
     directory = basename
-    parent_dir = "/home/allzero22/Webserver/webcam/static/video/"
-    output.fileoutput = (parent_dir + "Bird_%s.h264"  %directory)
+    output.fileoutput = OUTPUT_DIR_VIDEO / f"{directory}.h264"
     output.start()
-    
-    return render_template('startRec.html')
 
-@app.route('/stopRec.html')
+    return render_template("startRec.html")
+
+
+@app.route("/stopRec.html")
 def stopRec():
     """Stop Recording Pane"""
     print("Video Stop")
     output.stop()
-    
-    return render_template('stopRec.html')
 
-@app.route('/srecord.html')
+    return render_template("stopRec.html")
+
+
+@app.route("/srecord.html")
 def srecord():
     """Sound Record Pane"""
     print("Recording Sound")
-    timestamp = datetime.now().isoformat("_","seconds")
+    timestamp = datetime.now().isoformat("_", "seconds")
     print(timestamp)
-    subprocess.Popen('arecord -D dmic_sv -d 30 -f S32_LE /home/allzero22/Webserver/webcam/static/sound/birdcam_$(date "+%b-%d-%y-%I:%M:%S-%p").wav -c 2', shell=True)
-    
-    return render_template('srecord.html')
+    subprocess.Popen(
+        f'arecord -D dmic_sv -d 30 -f S32_LE {OUTPUT_DIR_SOUND}/cam_$(date "+%b-%d-%y-%I:%M:%S-%p").wav -c 2',
+        shell=True,
+    )
 
-@app.route('/snap.html')
+    return render_template("srecord.html")
+
+
+@app.route("/snap.html")
 def snap():
     """Snap Pane"""
     print("Taking a photo")
     camera.VideoSnap()
-    
-    return render_template('snap.html')
 
-api.add_resource(VideoFeed, '/cam')
+    return render_template("snap.html")
 
 
-if __name__ == '__main__':
-    app.run(debug = False, host = '0.0.0.0', port=5000)
+api.add_resource(VideoFeed, "/cam")
 
 
+if __name__ == "__main__":
+    OUTPUT_DIR_VIDEO.mkdir(exist_ok=True, parents=True)
+    OUTPUT_DIR_PICTURES.mkdir(exist_ok=True, parents=True)
+    OUTPUT_DIR_SOUND.mkdir(exist_ok=True, parents=True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
